2da237638e29cf6f755897f5fb87c7ff571b960b
anytodo92 <anything.todo92@gmail.com>
2023-03-30 10:09:43 +0200

    made changes on footer section

--- src/components/footers/Main/index.tsx
+++ src/components/footers/Main/index.tsx
@@ -13,27 +13,36 @@ const Footer = (): JSX.Element => {
         <div className="content">
           <div><img src={Images.WhiteLogoUrl} /></div>
           <div>
-            <div className="icon">
-              <img src={Icons.CallIconUrl} />
-              <div className="line"></div>
+            <div className="wrapper">
+              <div className="icon">
+                <img src={Icons.CallIconUrl} />
+                <div className="line"></div>
+              </div>
+              <div className="txt">
+                <p><a className="link" href="mailto:info@dynamicbodymassage.ch">info@dynamicbodymassage.ch</a></p>
+                <p><a className="link" href="tel:+41 079 123 45 67">+41 079 123 45 67</a></p>
+              </div>
             </div>
-            <p><a className="link" href="mailto:info@dynamicbodymassage.ch">info@dynamicbodymassage.ch</a></p>
-            <p><a className="link" href="tel:+41 079 123 45 67">+41 079 123 45 67</a></p>
+          </div>
+          <div>
+            <div className="wrapper">
+              <div className="icon">
+                <img src={Icons.SignupIconUrl} />
+                <div className="line"></div>
+              </div>
+              <div className="txt">
+                <p>
+                  Sportgym Aarburg<br />Wallgrabenweg 13<br />4663 Aarburg
+                </p>
+              </div>
+            </div>
           </div>
         </div>
-        <div>
-          <div className="icon">
-            <img src={Icons.SignupIconUrl} />
-            <div className="line"></div>
-          </div>
-          <p>
-            Sportgym Aarburg<br />Wallgrabenweg 13<br />4663 Aarburg
-          </p>
-        </div>
+        
       </section>
       <section>
         <div className="content">
-          <div>©Copyright 2023 - dynamic body massage</div>
+          <div class="txt">©Copyright 2023 - dynamic body massage</div>
           <div className="policy">
             <a className="link" href="/impressum">Impressum</a>
             <a className="link" href="/datenschutz">Datenschutz</a>
